f16c9a280d5ffe7265b2ebc6c00d7bdbec8982af
koki_hirose <masatoshousei@icloud.com>
2025-01-10 15:36:44 +0900

    add: handlerの追加

--- src/presentation/router.go
+++ src/presentation/router.go
@@ -1,20 +1,18 @@
 package presentation
 
-import "github.com/gin-gonic/gin"
+import (
+	"todo-app/presentation/handler"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Router() *gin.Engine {
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
 	{
-		v1.GET("/hello", hello)
+		v1.GET("/hello", handler.Hello)
 	}
 
 	return router
-}
-
-func hello(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "hello",
-	})
 }
